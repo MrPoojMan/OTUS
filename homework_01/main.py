86676dd2f2cf6eec9b98dfe6ee7906b34c2ef599
MrPoojMan <menzyrka2017@mail.ru>
2024-01-10 12:08:12 +0300

    FirstHomeWork3.0

--- homework_01/main.py
+++ homework_01/main.py
@@ -23,18 +23,28 @@ ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
-
-def filter_numbers(n, a):
+def is_prime(a):
+    if a % 2 == 0:
+        return a == 2
+    d = 3
+    while d * d <= a and a % d != 0:
+        d += 2
+    return d * d > a
+def filter_numbers(n, b):
     res = []
-    if a == 'even':
+    if b == 'even':
         for i in n:
             if i % 2 == 0:
                 res.append(i)
-    else:
-        a == 'odd'
+    elif b == 'odd':
         for i in n:
             if i % 2 != 0:
                 res.append(i)
+    else:
+        if b == 'prime':
+            for i in n:
+                if is_prime(i) == True:
+                    res.append(i)
     return res
 
 
--- main.py
+++ main.py
@@ -2,16 +2,30 @@ ODD = 'odd'
 EVEN = 'even'
 PRIME = 'prime'
 
-def filter_numbers(n, a):
+def is_prime(a):
+    if a % 2 == 0:
+        return a == 2
+    d = 3
+    while d * d <= a and a % d != 0:
+        d += 2
+    return d * d > a
+def filter_numbers(n, b):
     res = []
-    if a == 'even':
+    if b == 'even':
         for i in n:
             if i % 2 == 0:
                 res.append(i)
-    else:
-        a == 'odd'
+    elif b == 'odd':
         for i in n:
             if i % 2 != 0:
                 res.append(i)
+    else:
+        if b == 'prime':
+            for i in n:
+                if is_prime(i) == True:
+                    res.append(i)
     return res
 
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 61]
+
+print(filter_numbers(a, PRIME))
